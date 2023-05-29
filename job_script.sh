aa625beb7be6ab42b64dfdbb3a4fd4a336c9cca0
NikolasVellnow <nikolasvellnow@gmail.com>
2023-05-29 17:12:53 +0200

    changed job_script to test on server

--- job_script.sh
+++ job_script.sh
@@ -2,7 +2,7 @@
 #SBATCH --partition=short
 #SBATCH --nodes=4
 #SBATCH --ntasks-per-node=1
-#SBATCH --time=02:00:00 
+#SBATCH --time=00:06:00 
 #SBATCH --cpus-per-task=16
 #SBATCH --mem-per-cpu=12G
 #SBATCH --job-name=kraken_job
@@ -12,13 +12,13 @@
 conda activate kraken
 
 DB_NAME=PlusPFP-16
-FILE_NAME=s5_EKDN230004354-1A_HNHMFDSX5_L3_sorted_dedup.bam
+FILE_NAME=s5_EKDN230004354-1A_HNHMFDSX5_L3_sorted_dedup_subset.bam
 DB_PATH=/scratch/mnikvell/kraken_job/${DB_NAME}/
 
 OUT_PATH=/scratch/mnikvell/kraken_job/kraken_outputs/
 OUTPUT_NAME=output_${FILE_NAME}_${DB_NAME}
 REPORT_NAME=report_${FILE_NAME}_${DB_NAME}
-FILE_PATH=/work/mnikvell/data/mapped_reads/
+FILE_PATH=/work/mnikvell/data/mapped_reads/subsets/
 
 
 # create directories in scratch-dir
@@ -26,10 +26,10 @@ mkdir /scratch/mnikvell/kraken_job/
 mkdir /scratch/mnikvell/kraken_job/kraken_outputs
 
 # move database to scratch-dir
-mv /work/mnikvell/data/Kraken2/dbs/${DB_NAME} /scratch/mnikvell/kraken_job/
+cp -a /work/mnikvell/data/Kraken2/dbs/${DB_NAME} /scratch/mnikvell/kraken_job/
 
 # move file to scratch-dir
-mv ${FILE_PATH}${FILE_NAME} /scratch/mnikvell/kraken_job/
+cp ${FILE_PATH}${FILE_NAME} /scratch/mnikvell/kraken_job/
 
 # move to job directory
 cd scratch/mnikvell/kraken_job/
@@ -39,6 +39,7 @@ kraken2 \
 --output "${OUT_PATH}${OUTPUT_NAME}" \
 --use-names \
 --report "${OUT_PATH}${REPORT_NAME}" \
+--threads 16 \
 "${FILE_NAME}" 
 
 
