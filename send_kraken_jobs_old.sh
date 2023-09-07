b5409316b6903b233338c351c7b87ab47e758a1e
NikolasVellnow <nikolasvellnow@gmail.com>
2023-09-07 09:55:13 +0200

    writing scripts to send kraken classification jobs

--- send_kraken_jobs_old.sh
+++ send_kraken_jobs_old.sh
@@ -7,7 +7,7 @@ SCRIPT_PATH=${PWD}
 cd ${INPUT_PATH}
 for file in *_unmapped.fasta
 do
-	sbatch "${SCRIPT_PATH}/job_script_kraken_classification.sh" ${file}
+	sbatch "${SCRIPT_PATH}/job_script_kraken.sh" ${file}
 
 done
 	
