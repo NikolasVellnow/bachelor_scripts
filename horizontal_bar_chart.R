e20e8361f52b599b7fd35013c1b50c636a13da08
NikolasVellnow <42894685+NikolasVellnow@users.noreply.github.com>
2023-09-08 14:51:48 +0200

    improved color palette

--- horizontal_bar_chart.R
+++ horizontal_bar_chart.R
@@ -1,11 +1,14 @@
 library(ggplot2)
 library(readxl)
+library(viridis)
 
 path <- "~/sciebo/Bioinformatik Bachelor Bielefeld/7_SS_2023/bachelor_project/"
 kingdoms <- read_excel(paste(path, "kraken_taxa_percentages.xlsx", sep = ""), sheet = "kingdoms")
 
 
 
-p <- ggplot(kingdoms, aes(x = sample, y = percent_of_reads))+
-    geom_col(aes(fill = taxon), width = 0.7) + coord_flip()
+p <- ggplot(kingdoms, aes(x = sample, y = percent_of_reads)) +
+    geom_col(aes(fill = taxon), width = 0.7) +
+    scale_fill_viridis(discrete = TRUE, option = "D") +
+    coord_flip()
 p
